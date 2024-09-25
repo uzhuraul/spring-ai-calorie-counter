31ca943d6daf1dc25a545dbaff6da3f501a280e3
uzhuraul <uladzislau.zhurauliou@gmail.com>
2024-09-25 14:46:34 +0200

    Init commit

---
+++ src/main/java/uladzislau/zhurauliou/spring_ai_calorie_counter/SpringAiCalorieCounterApplication.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_calorie_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiCalorieCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiCalorieCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uladzislau/zhurauliou/spring_ai_calorie_counter/SpringAiCalorieCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_calorie_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiCalorieCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
